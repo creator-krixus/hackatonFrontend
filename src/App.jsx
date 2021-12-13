6c279d9abbcd0ce2147872eefa2da5c9d16537bf
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2021-12-13 08:36:49 -0500

    Feature: Modified import Header and create LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
+import LandingPage from './components/landingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <LandingPage/>
     </div>
   );
 }
