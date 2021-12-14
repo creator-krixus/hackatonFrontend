110abadb7da3ae20c014d6177e8a21f870a3e4a3
creator-krixus <wilsonrueda2@gmail.com>
2021-12-14 11:21:56 -0500

    Feature: add routes of login and register

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Header from './components/header/Header';
 import LandingPage from './components/landingPage/LandingPage';
+import Register from './components/register/Register'
+import Login from './components/login/Login'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </div>
   );
