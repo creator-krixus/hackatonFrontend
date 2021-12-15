af73e892980b1bcfc46096d253194809ddc5aa79
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-12-14 22:39:53 -0500

    Fix: Registrate modification button done

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ import Login from './components/login/Login'
 import Financiero from "./components/financiero/Financiero";
 import Educacion from "./components/educacion/Educacion";
 import Registrate from "./components/registrate/Registrate";
-import Comunidad from "./components/comunidad/Comunidad";
-import Footer from './components/footer/Footer'
+
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Registrate />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
-      <Footer />
-    </div>
+     </div>
   );
 }
 
--- src/components/registrate/Registrate.jsx
+++ src/components/registrate/Registrate.jsx
@@ -1,6 +1,5 @@
 import React, {useRef, useEffect} from 'react'
 import './Registrate.css';
-import picos from '../../assets/components/peaksgreen.png';
 import { TweenMax,Power3 } from 'gsap';
 import { Link } from 'react-router-dom';
 
@@ -9,27 +8,23 @@ import { Link } from 'react-router-dom';
 
 export default function Registrate() {
 
-   let ImgRegist = useRef(null);
+ 
    let textItem = useRef(null);
 
    
    useEffect(() =>{
 
       
-      console.log(ImgRegist);
-      TweenMax.to(ImgRegist,.8,{opacity:1,y:1, ease:Power3.easeOut,delay:1})
       TweenMax.to(textItem,.8,{opacity:1, y:1, ease:Power3.easeOut, delay:1})},[])
    
     return (
-       <div>
-           
-               <h3 ref={el =>{textItem = el}} className="last-title">Entre muchas otras herramientas. ¡Regístrate!</h3>
-               <img ref={el =>{ImgRegist = el}} src= {picos} alt="Green peaks" className="down" />
-               <div className="register-container">
-                  <h3 className="last-title">Entre muchas otras herramientas. ¡Regístrate!</h3>
+       <div className='registrate-cont'>
+           <div>
+               <h3 className='paragraph' ref={el =>{textItem = el}} >Entre muchas otras herramientas. ¡Regístrate!</h3>
+               <div           className="register-container">                 
                   <button className="secondary-button register"><Link to="register">Regístrate</Link></button>
                </div>
-           
+            </div>
       </div>
    )
 }
