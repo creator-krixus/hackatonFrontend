e4635ed5e2a6bcb9aad72fc2d462a824eedf97c2
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-12-15 10:45:11 -0500

    Feature: agendar component done

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import Login from './components/login/Login'
 import Financiero from "./components/financiero/Financiero";
 import Emocional from './components/emocional/Emocional'
 import Laboral from './components/laboral/Laboral'
-
+import Registrate from './components/registrate/Registrate'
+import Agendar from './components/agendar/Agendar'
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+           
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="login" element={<Login />} />
@@ -21,6 +23,8 @@ function App() {
         <Route path="financiero" element={<Financiero />} />
         <Route path="emocional" element={<Emocional />} />
         <Route path="laboral" element={<Laboral />} />
+        <Route path="agendar" element={<Agendar />} />
+        <Route path="registrate" element={<Registrate />} />
       </Routes>
      </div>
   );
