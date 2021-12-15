1d941991ffbc797debaa6e4a3f83f27af9e42d3f
creator-krixus <wilsonrueda2@gmail.com>
2021-12-14 22:45:33 -0500

    Feature: Color header dinamico

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import menu from '../../assets/icons/bars-solid.svg';
 import icon from '../../assets/icons/meteor-solid.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css'
 
 export default function Header() {
+
+    const location = useLocation();
+    const ruta = location.pathname
+    const idRuta = ruta.substring(1,20)
+    let color = {}
+
+    if(idRuta === 'emocional'){
+        color = {background: '#FF7878'}
+    }
+    if(idRuta === 'financiero'){
+        color = {background: '#507445'}
+    }
+
     return (
         <div>
-            <nav className="navbar">
+            <nav className="navbar" style={color}>
             <div className="navbar-logo">
                 <Link to="/"><img src={icon} alt="Logo" className="navbar-logo"/> 
                 </Link>
