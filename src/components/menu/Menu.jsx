9fc833f0c8f3a42f57ea754ec9eda2f5bb5cf372
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-12-15 23:11:29 -0500

    Fix: menu Style

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import './Menu.css'
 import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap'
+import girls from '../../assets/images/girls.png'
 
 
 export default function Menu() {
@@ -14,17 +15,19 @@ export default function Menu() {
 
         <div className="container-emo">
             <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown}> 
-                <DropdownToggle header>
-                    Menu
+                <DropdownToggle header className='label'>
+                    <label htmlFor="btn-menu" className='btn-menu'>
+                        <img className="test--img image" src={girls} alt="Work from home" />
+                    </label>  
                 </DropdownToggle>
 
-                <DropdownMenu>  
-                   <DropdownItem> Laboral </DropdownItem>
-                    <DropdownItem> Emocional </DropdownItem>
-                    <DropdownItem> Encuesta </DropdownItem>
-                    <DropdownItem>  Registate </DropdownItem>
-                    <DropdownItem> uno </DropdownItem>
-                    <DropdownItem> otro</DropdownItem>
+                <DropdownMenu className='menu'>
+                    <DropdownItem className='boton'> Laboral </DropdownItem>
+                    <DropdownItem className='boton'> Emocional </DropdownItem>
+                    <DropdownItem className='boton'> Encuesta </DropdownItem>
+                    <DropdownItem className='boton'>  Registate </DropdownItem>
+                    <DropdownItem className='boton'> uno </DropdownItem>
+                    <DropdownItem className='boton'> otro</DropdownItem>
                 </DropdownMenu>                
             
             
