eceed3ab38905abc20b4376dc913b1223e78f18c
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-12-15 21:23:30 -0500

    Feature: Menu Dropdown done sin css

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+import './Menu.css'
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap'
+
+
+export default function Menu() {
+
+            const [dropdown, setDropdown,] = useState(false);
+            const abrirCerrarDropdown=()=>{
+                setDropdown(!dropdown);
+            }
+     
+    return (
+
+        <div className="container-emo">
+            <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown}> 
+                <DropdownToggle>
+                    Dropdown ejemplo
+                </DropdownToggle>
+
+                <DropdownMenu>
+                    <DropdownItem> Financiero </DropdownItem>
+                    <DropdownItem> Laboral </DropdownItem>
+                    <DropdownItem> Emocional </DropdownItem>
+                    <DropdownItem> Encuesta </DropdownItem>
+                    <DropdownItem>  Registate </DropdownItem>
+                    <DropdownItem> uno </DropdownItem>
+                    <DropdownItem> otro</DropdownItem>
+                </DropdownMenu>                
+            
+            
+            </Dropdown>            
+        </div>
+    )
+}
